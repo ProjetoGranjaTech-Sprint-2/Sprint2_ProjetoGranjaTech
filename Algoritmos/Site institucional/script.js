0836a386beef36ac9bb529a4999b9dfd0e4f6b86
LuisGustavoDAlmeida <luis.almeida@sptech.school>
2023-10-15 23:35:00 -0300

    Header móvel, mudança section 1 e adição 2 sections, correção simulador

--- Algoritmos/Site institucional/script.js
+++ Algoritmos/Site institucional/script.js
@@ -4,14 +4,14 @@ function calcular() {
     var qtdOvo = Number(input_qtd_ovo.value);
     var precoOvo = Number(input_preco_ovo.value);
     // variáveis dos inputs
-    var galinhasPerdidas = (galinhasMortas / galinhas) * 100 // Média de galinhas perdidas
-    var ovoPorGalinha = qtdOvo / galinhas  // Ovos gerados por galinha
-    var ovoPorGalinhaPerdido = ovoPorGalinha * galinhasMortas // Quantidade de ovos perdidos por galinha 
-    var lucro = qtdOvo * precoOvo // Lucro em cima da quantidade de ovos e do preço do ovo
-    var lucroPerdido = ovoPorGalinhaPerdido * precoOvo  // média de ovos por galinha perdidas pelo preço do ovo unitário gera o lucro perdido
+    var galinhasPerdidas = (galinhasMortas / galinhas) * 100; // Média de galinhas perdidas
+    var ovoPorGalinha = qtdOvo / galinhas;  // Ovos gerados por galinha
+    var ovoPorGalinhaPerdido = ovoPorGalinha * galinhasMortas; // Quantidade de ovos perdidos por galinha 
+    var lucro = qtdOvo * precoOvo; // Lucro em cima da quantidade de ovos e do preço do ovo
+    var lucroPerdido = ovoPorGalinhaPerdido * precoOvo;  // média de ovos por galinha perdidas pelo preço do ovo unitário gera o lucro perdido
     div_html.innerHTML = `
     <span> 
-        De suas <b style="color: #4A3D33;"> ${galinhas} </b> galinhas, em média <b style="color: #4A3D33;"> ${(galinhasPerdidas).toFixed(3)}%. </b> delas morrem por falta de monitoramento de regulação da temperatura e umidade, com <b style="color: #4A3D33;"> ${galinhasMortas} </b> galinhas a menos, devido a perda de <b style="color: #4A3D33;">${(ovoPorGalinhaPerdido).toFixed(0)} </b> ovos, isso faz você deixar de ganhar:
+        De suas <b style="color: #4A3D33;"> ${galinhas} </b> galinhas, em média <b style="color: #4A3D33;"> ${(galinhasPerdidas).toFixed(0)}%. </b> delas morrem por falta de monitoramento de regulação da temperatura e umidade, com <b style="color: #4A3D33;"> ${galinhasMortas} </b> galinhas a menos, devido a perda de <b style="color: #4A3D33;">${(ovoPorGalinhaPerdido).toFixed(0)} </b> ovos, isso faz você deixar de ganhar:
     </span>
     <h2 style="color: #4A3D33;">R$ ${(lucroPerdido).toFixed(2)} mensalmente </h2> 
     <h2 style="color: #4A3D33;">R$ ${(lucroPerdido * 6).toFixed(2)} a cada 6 meses </h2>
