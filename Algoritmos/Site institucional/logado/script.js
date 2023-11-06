2aa5d453661e403f28a95af252c54f53718ec32e
Gabriel Garcia <gabriel.goliveira@sptech.school>
2023-11-06 17:20:24 -0300

    Arrumei os erros de hortografia no site

--- Algoritmos/Site institucional/logado/script.js
+++ Algoritmos/Site institucional/logado/script.js
@@ -11,7 +11,7 @@ function calcular() {
     var lucroPerdido = ovoPorGalinhaPerdido * precoOvo;  // média de ovos por galinha perdidas pelo preço do ovo unitário gera o lucro perdido
     div_html.innerHTML = `
     <span> 
-        De suas <b style="color: #ffd890;"> ${galinhas} </b> galinhas, em média <b style="color: #ffd890;"> ${(galinhasPerdidas).toFixed(0)}%</b> delas morrem por falta de monitoramento de regulação da temperatura e umidade, com <b style="color: #ffd890;"> ${galinhasMortas} </b> galinhas a menos, devido a perda de <b style="color: #ffd890;">${(ovoPorGalinhaPerdido).toFixed(0)} </b> ovos, isso faz você deixar de ganhar:
+    Das suas<b style="color: #ffd890;"> ${galinhas} </b> galinhas, em média <b style="color: #ffd890;"> ${(galinhasPerdidas).toFixed(0)}%</b> delas morrem devido à falta de regulação da temperatura e umidade. Isso resulta em <b style="color: #ffd890;"> ${galinhasMortas} </b> galinhas a menos, e na perda de  <b style="color: #ffd890;">${(ovoPorGalinhaPerdido).toFixed(0)} </b> ovos, isso faz com que você deixe de ganhar:
     </span>
     <h2 style="color: #ffd890;">R$ ${(lucroPerdido).toFixed(2)} mensalmente </h2> 
     <h2 style="color: #ffd890;">R$ ${(lucroPerdido * 6).toFixed(2)} a cada 6 meses </h2>
