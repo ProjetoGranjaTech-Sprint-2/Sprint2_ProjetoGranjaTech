f20880582535b05a6543bce4f4645600b7c76919
thiagovieiras <thiago.santos@sptech.school>
2023-11-29 18:59:29 -0300

    fiz o fetch para realizar a autenticação do login

--- granjaTech/public/js/login.js
+++ granjaTech/public/js/login.js
@@ -18,31 +18,70 @@ function entrar() {
                     senha.indexOf('$') >= 0 ||
                     senha.indexOf('&') >= 0 ||
                     senha.indexOf('*') >= 0;
+                if (possui_especial) {
 
-                img_galinha.style.display = 'block'; // Mostra a imagem da galinha
-                setTimeout(function () {
-                    img_galinha.style.animation = 'spin 2s linear infinite'; // Adicione a animação
-                    setTimeout(function () {
-                        window.location.href = "../logado/logado.html";
-                    }, 900); // Redireciona após a animação ter girado por um tempo
-                }, 10); // Aguarde um curto período antes de mostrar e animar a galinha
+                    fetch("/usuarios/autenticar", {
+                        method: "POST",
+                        headers: {
+                            "Content-Type": "application/json"
+                        },
+                        body: JSON.stringify({
+                            emailServer: emailVar,
+                            senhaServer: senhaVar
+                        })
+                    })
+                        .then(function (resposta) {
+                            console.log("ESTOU NO THEN DO ENTRAR()!");
+
+                            if (resposta.ok) {
+                                console.log(resposta);
+
+                                resposta.json().then(json => {
+                                    console.log(json);
+                                    console.log(JSON.stringify(json));
+                                    sessionStorage.EMAIL_USUARIO = json.email;
+                                    sessionStorage.NOME_USUARIO = json.nome;
+                                    sessionStorage.ID_USUARIO = json.id;
+
+
+                                    img_galinha.style.display = 'block'; // Mostra a imagem da galinha
+                                    setTimeout(function () {
+                                        img_galinha.style.animation = 'spin 2s linear infinite'; // Adicione a animação
+                                        setTimeout(function () {
+                                            window.location.href = "../logado/logado.html";
+                                        }, 900); // Redireciona após a animação ter girado por um tempo
+                                    }, 10); // Aguarde um curto período antes de mostrar e animar a galinha
+                                });
+                            } else {
+                                console.log("Houve um erro ao tentar realizar o login!");
+
+                                resposta.text().then(texto => {
+                                    console.error(texto);
+                                    finalizarAguardar(texto);
+                                });
+                            }
+                        }).catch(function (erro) {
+                            console.log(erro)
+                        });
+                    return false;
+                }
             }
+        } else if (login == "" || senha == "") {
+            input_login.value = '';
+            input_senha.value = '';
+            input_login.focus();
+            mensagem_erro.innerHTML = `Usuário ou senha incorretos.`;
+        } else {
+            input_login.value = '';
+            input_senha.value = '';
+            input_login.focus();
+            mensagem_erro.innerHTML = `Usuário ou senha incorretos.`;
         }
-    } else if (login == "" || senha == "") {
-        input_login.value = '';
-        input_senha.value = '';
-        input_login.focus();
-        mensagem_erro.innerHTML = `Usuário ou senha incorretos.`;
-    } else {
-        input_login.value = '';
-        input_senha.value = '';
-        input_login.focus();
-        mensagem_erro.innerHTML = `Usuário ou senha incorretos.`;
     }
 }
+
 function checkEnter(event) {
     if (event.key === "Enter") {
         entrar();
     }
 }
-
