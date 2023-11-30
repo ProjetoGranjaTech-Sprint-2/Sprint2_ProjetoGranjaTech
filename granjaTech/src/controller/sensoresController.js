be95e5d3d86d7a8d6bea39b6107a826298718a2f
LuisGustavoDAlmeida <luis.almeida@sptech.school>
2023-11-30 01:34:57 -0300

    Fiz a rota e plotei os gráficos na dash e criei outro gitignore

--- granjaTech/src/controller/sensoresController.js
+++ granjaTech/src/controller/sensoresController.js
@@ -2,17 +2,29 @@ var sensoresModel = require("../models/sensoresModel");
 
 function listarSensor(req, res) {
     sensoresModel.listarSensor()
-    .then((resultado) => {
-        res.status(200).json(resultado);
-    })
+        .then((resultado) => {
+            res.status(200).json(resultado);
+        })
         // res.json(resultado)
         .catch((erro) => {
             console.error(`Erro ao listar sensores: ${erro}`);
             res.status(500).json({ erro: "Erro interno ao listar sensores" });
         });
-    };
+};
+
+function dadosSensor(req, res) {
+    sensoresModel.dadosSensor()
+        .then((resultado) => {
+            res.status(200).json(resultado);
+        })
+        .catch((erro) => {
+            console.error(`Erro ao ler os valores sensores: ${erro}`);
+            res.status(500).json({ erro: "Erro interno ao ler os sensores" });
+        })
+}
 
 module.exports = {
     listarSensor,
+    dadosSensor,
 }
 
