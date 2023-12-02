af7fadaf07b643e369ca4873b3d8d000f2c62727
thiagovieiras <thiago.santos@sptech.school>
2023-12-02 18:33:35 -0300

    função de enviar email arrumada

--- granjaTech/public/js/ajuda.js
+++ granjaTech/public/js/ajuda.js
@@ -1,7 +1,8 @@
 function enviar() {
     
     var texto = textarea.value;
-    var assunto = slc_category.value
+    var assunto = slc_category.value;
+    var email = sessionStorage.EMAIL_USUARIO;
     if (texto == '') alert("ERRO - Não reconhecemos sua solicitação, verifique o campo");
     else {
 
@@ -10,15 +11,25 @@ function enviar() {
             headers: {
                 'Content-Type' : 'application/json',
             },
-            body: JSON.stringify({ assunto, texto }),
+            body: JSON.stringify({ assunto, texto, email}),
         })
-        .then(response => response.json())
-        .then(data => {
+        .then(response => {
+            response.json()
             console.log(data);
-            finalizar();
         })
         .catch(error => {
             console.log('Erro:', error)
         })         
+        finalizar();
     }
 };
+
+function finalizar() {
+    textarea.value = ''
+    cardErro.style.display = "block";
+    mensagem_erro.innerHTML = `Cadastro realizado com sucesso! Redirecionando para tela de Login...`;
+    setTimeout(() => {
+        cardErro.style.display = "none";
+    }, 2500);
+}
+
--- granjaTech/src/routes/dashboard.js
+++ granjaTech/src/routes/dashboard.js
@@ -5,46 +5,63 @@ var router = express.Router();
 var dashboardController = require('../controller/dashboardController.js')
 
 router.post("/inserir/galpoes", function (req, res) {
-    dashboardController.inserirCliente(req, res);  
+    dashboardController.inserirCliente(req, res);
 });
 
 router.post("/excluir/galpoes", function (req, res) {
-    dashboardController.excluirTupla(req, res);  
+    dashboardController.excluirTupla(req, res);
 });
 
 router.post("/selecionar/galpoes", function (req, res) {
-    dashboardController.selecionarGalpao(req, res);  
+    dashboardController.selecionarGalpao(req, res);
 });
 
 router.post("/enviar-email", (req, res) => {
 
-    var { assunto, texto } = req.body;
+    var { assunto, texto, email } = req.body;
 
     // Configurar o transporte
     const transporter = nodemailer.createTransport({
         service: 'outlook',
         auth: {
-            user: 'thythy04@outlook.com',
-            pass: 'Mari2013',
+            user: 'auto-granjatech@outlook.com',
+            pass: 'AutoGranjatech',
         },
     });
 
-        // Definir detalhes do e-mail
-        const mailOptions = {
-        from: 'thythy04@outlook.com',
+    // Definir detalhes do e-mail
+    const mailOptions = {
+        from: 'auto-granjatech@outlook.com',
         to: 'help@granjatech.on.spiceworks.com',
         subject: assunto,
-        text: texto,
-        };
+        html: `
+            <html>
+            <head>
+                <style>
+                body {
+                    background-color: #f0f0f0;
+                    color: #333;
+                    font-family: Arial, sans-serif;
+                }
+                </style>
+            </head>
+            <body>
+                <h1>Sou, ${email}</h1>
+                <p>${texto}</p>
+                <p>Você pode personalizar o conteúdo aqui.</p>
+            </body>
+            </html>
+        `,
+    };
 
-        // Enviar o e-mail
-        transporter.sendMail(mailOptions, function(error, info){
+    // Enviar o e-mail
+    transporter.sendMail(mailOptions, function (error, info) {
         if (error) {
             console.error('Deu' + error);
         } else {
             console.log('Email enviado: ' + info.response);
         }
-    });      
+    });
 });
 
 module.exports = router;
