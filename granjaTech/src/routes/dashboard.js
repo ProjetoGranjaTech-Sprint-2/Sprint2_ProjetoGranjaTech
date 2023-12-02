1a125e2df644dbeceab6b174358bcc75d6cb48f9
thiagovieiras <thiago.santos@sptech.school>
2023-12-02 17:46:51 -0300

    finalizei a função de enviar a email

--- granjaTech/src/routes/dashboard.js
+++ granjaTech/src/routes/dashboard.js
@@ -1,4 +1,5 @@
 var express = require("express");
+var nodemailer = require("nodemailer");
 var router = express.Router();
 
 var dashboardController = require('../controller/dashboardController.js')
@@ -13,6 +14,37 @@ router.post("/excluir/galpoes", function (req, res) {
 
 router.post("/selecionar/galpoes", function (req, res) {
     dashboardController.selecionarGalpao(req, res);  
+});
+
+router.post("/enviar-email", (req, res) => {
+
+    var { assunto, texto } = req.body;
+
+    // Configurar o transporte
+    const transporter = nodemailer.createTransport({
+        service: 'outlook',
+        auth: {
+            user: 'thythy04@outlook.com',
+            pass: 'Mari2013',
+        },
+    });
+
+        // Definir detalhes do e-mail
+        const mailOptions = {
+        from: 'thythy04@outlook.com',
+        to: 'help@granjatech.on.spiceworks.com',
+        subject: assunto,
+        text: texto,
+        };
+
+        // Enviar o e-mail
+        transporter.sendMail(mailOptions, function(error, info){
+        if (error) {
+            console.error('Deu' + error);
+        } else {
+            console.log('Email enviado: ' + info.response);
+        }
+    });      
 });
 
 module.exports = router;
